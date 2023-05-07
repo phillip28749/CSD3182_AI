897d9615af2bec5f1e25c1955bb816154b3e754d
phillip28749 <phillip.c.azrael@gmail.com>
2023-05-07 09:10:33 +0800

    function.cpp/h update

--- cs380_a1/cs380_a1/functions.cpp
+++ cs380_a1/cs380_a1/functions.cpp
@@ -1,6 +1,84 @@
+/************************************************************************************//*!
+\file          functions.cpp
+\project       A1
+\author        Chen Yen Hsun, 20027671
+\par           email: c.yenhsun\@digipen.edu
+\date          May 7, 2023
+\brief
+
+
+
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
 #include "functions.h"
 
 namespace AI 
 {
- 
+    template<typename T>
+    bool Node<T>::isnumber(const std::string& str) {
+        for (auto x : str) {
+            if (!std::isdigit(x))
+                return false;
+        }
+        return true;
+    }
+
+    template<typename T>
+    std::string Node<T>::getAsString() {
+
+        std::string str;
+        str += value + " {";
+        str += std::to_string(children.size()) + " ";
+        for (const auto& child : children)
+            str += child->getAsString();
+        str += "} ";
+        return str;
+    }
+
+    template<typename T>
+    Node<T>* BFS(Node<T>& node, const T& lookingfor)
+    {
+        std::queue<Node<T>*> openlist;
+        openlist.push(&node);
+        while (!openlist.empty())
+        {
+            Node<T>* current = openlist.front();
+            openlist.pop();
+            if (current->value == lookingfor)
+                return current;
+            for (Node<T>* child : current->children)
+                openlist.push(child);
+        }
+        return nullptr;
+    }
+
+    template<typename T>
+    Node<T>* DFS(Node<T>& node, const T& lookingfor)
+    {
+        std::stack<Node<T>*> openlist;
+        openlist.push(&node);
+        while (!openlist.empty())
+        {
+            Node<T>* current = openlist.top();
+            openlist.pop();
+            if (current->value == lookingfor)
+                return current;
+            for (Node<T>* child : current->children)
+                openlist.push(child);
+        }
+        return nullptr;
+    }
+
+    // Explicit instantiation of the required types
+    template bool Node<int>::isnumber(const std::string&);
+    template bool Node<std::string>::isnumber(const std::string&);
+    template std::string AI::Node<int>::getAsString();
+    template std::string AI::Node<std::string>::getAsString();
+    template Node<std::string>* BFS(Node<std::string>& node, const std::string& lookingfor);
+    template Node<std::string>* DFS(Node<std::string>& node, const std::string& lookingfor);
 } 
+
--- cs380_a1/cs380_a1/functions.h
+++ cs380_a1/cs380_a1/functions.h
@@ -1,3 +1,19 @@
+/************************************************************************************//*!
+\file          functions.h
+\project       A1
+\author        Chen Yen Hsun, 20027671
+\par           email: c.yenhsun\@digipen.edu
+\date          May 7, 2023
+\brief
+               
+
+
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*//*************************************************************************************/
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -42,16 +58,22 @@ namespace AI
         // Serialization
         friend std::ostream& operator<<(std::ostream& os, Node const& rhs)
         {
-            os << rhs.value; // just for test purpose
-            // ...
+            os << rhs.value << " {";
+            os << rhs.children.size() << ' ';
+            for (auto const& child : rhs.children) {
+                os << child->getAsString();
+            }
+            os << "} ";
+
             return os;
         }
 
         // Deserialization
         friend std::istream& operator>>(std::istream& is, Node& rhs)
         {
-            is >> rhs.value; // just for test purpose
-            // ...
+            std::string input;
+            std::getline(is, input);
+            rhs.setFromString(input);
             return is;
         }
 
@@ -59,30 +81,81 @@ namespace AI
         std::vector<T> getPath() const
         {
             std::vector<T> r;
-            // ...
+            r.push_back(this->value);
+            Node* node = this->parent;
+            while (node)
+            {
+                r.insert(r.begin(), node->value);
+                node = node->parent;
+            }
             return r;
         }
+
+        // Deserialization function that turns a stream of bytes 
+        // into an object in memory.
+        std::string setFromString(std::string str)
+        {
+
+            size_t n = str.find(' ');
+
+            if (n == std::string::npos)
+                return str;
+
+            this->value = str.substr(0, n);
+            str = str.substr(n + 1);
+
+            if (str[0] != '{')
+                return str;
+
+            str = str.substr(1);
+
+            n = str.find(' ');
+            if (n == std::string::npos)
+                return str;
+
+            int length;
+            bool check = isnumber(str.substr(0, n));
+
+            if (!check) 
+                return str;
+            else 
+                length = std::stoi(str.substr(0, n));
+           
+            str = str.substr(n + 1);
+
+            for (int i = 0; i < length; ++i)
+            {
+                Node* child = new Node("", this);
+                str = child->setFromString(str);
+                this->children.push_back(child);
+            }
+
+            n = str.find('}');
+            if (n == std::string::npos)
+                return str;
+
+            str = str.substr(n + 2);
+
+            return str;
+        }
+
+        // Serialization function that turn a tree in memory into a stream  
+        // of bytes so it can be stored on disk or sent over the network.
+        std::string getAsString();
+
+        //Helper function
+        static bool isnumber(const std::string& str);
     };
 
     // Implementation of the Breadth-First Search algorithm
     template<typename T>
-    Node<T>* BFS(Node<T> & node, const T & lookingfor)
-    {
-        UNUSED(node)
-        UNUSED(lookingfor)
-        // ...
-        return nullptr;
-    }
+    Node<T>* BFS(Node<T>& node, const T& lookingfor);
+
 
     // Implementation of the Depth-First Search algorithm
     template<typename T>
-    Node<T>* DFS(Node<T> & node, const T & lookingfor)
-    {
-        UNUSED(node)
-        UNUSED(lookingfor)
-        // ...
-        return nullptr;
-    }
+    Node<T>* DFS(Node<T>& node, const T& lookingfor);
+
 
 } // end namespace
 
