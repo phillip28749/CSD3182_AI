e6adf92d8e93a5082020703c4bc8952d8e6e557b
phillip28749 <phillip.c.azrael@gmail.com>
2023-05-21 06:02:41 +0800

    update a3

--- cs380_a3/functions.cpp
+++ cs380_a3/functions.cpp
@@ -1,7 +1,164 @@
+/*!*****************************************************************************
+\file functions.cpp
+\author Chen Yen Hsun
+\par DP email: c.yenhsun\@digipen.edu
+\par Course: CS380
+\par Section: A
+\par Programming Assignment 3
+\date 05-21-2023
+\brie
+
+Conbinaion of assignment 1 and 2, define function declared in functions.h
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*******************************************************************************/
 #include "functions.h"
 
 namespace AI 
 {
 
+    /*!*****************************************************************************
+    \brief
+    Converts the node and its children into a string representation.
 
-} 
+    \return
+    Returns a string representation of the node and its children.
+    *******************************************************************************/
+    template<typename T>
+    std::string Node<T>::getAsString() {
+
+        std::string str;
+        str += value + " {";
+        str += std::to_string(children.size()) + " ";
+        for (const auto& child : children)
+            str += child->getAsString();
+        str += "} ";
+        return str;
+    }
+    template std::string AI::Node<int>::getAsString();
+    template std::string AI::Node<std::string>::getAsString();
+
+    /*!*****************************************************************************
+    \brief
+    Checks whether a given string represents a number.
+
+    \param str
+    The string to be checked.
+
+    \return
+    Returns true if the string represents a number; otherwise, returns false.
+    *******************************************************************************/
+    template<typename T>
+    bool Node<T>::isnumber(const std::string& str) {
+        for (auto x : str) {
+            if (!std::isdigit(x))
+                return false;
+        }
+        return true;
+    }
+    template bool Node<int>::isnumber(const std::string&);
+    template bool Node<std::string>::isnumber(const std::string&);
+
+    /*!*****************************************************************************
+    \brief
+        // Push to the list all children of pNode excluding
+        // those with value not equal "x"
+
+    \param TreeNode* pNode
+        node to be pushed
+    \return
+        A vector of TreeNode that contain the pushed node
+    *******************************************************************************/
+    std::vector<TreeNode*> GetTreeAdjacents::operator()(TreeNode* pNode)
+    {
+        std::vector<AI::TreeNode*> list = {};
+
+        for (auto node : pNode->children) {
+
+            if (node->value == "x")
+                list.emplace_back(node);
+        }
+
+        return list;
+    }
+
+    /*!*****************************************************************************
+     * \brief
+        Returns a vector of shuffled adjacent nodes in a map.
+     *
+     * \param pNode
+        The TreeNode of the node whose adjacent nodes are to be retrieved.
+     *
+     * \return
+        A vector of pointers to shuffled adjacent nodes in the map.
+    *******************************************************************************/
+    std::vector<TreeNode*> GetTreeStochasticAdjacents::operator()(TreeNode* pNode)
+    {
+            std::vector<TreeNode*> adjacents;
+            adjacents = GetTreeAdjacents::operator()(pNode);
+
+            // Use the base class operator() and then shuffle the result
+            std::random_shuffle(adjacents.begin(), adjacents.end());
+
+            return adjacents;
+    }
+
+    /*!*****************************************************************************
+     * \brief
+        Runs the flood fill algorithm recursively.
+     *
+     * \param pNode
+        The pNode of the starting node for flood fill.
+
+     * \param value
+        The value to fill the connected nodes with.
+    *******************************************************************************/
+    void Flood_Fill_Recursive::run(TreeNode* pNode, std::string value)
+    {
+        // Implement the flood fill
+        std::vector<TreeNode*> adjacents = pGetAdjacents->operator()(pNode);
+
+        for (TreeNode* adj : adjacents)
+        {
+            adj->value = value;
+            run(adj, value);
+            //delete adj;
+        }
+    }
+
+    /*!*****************************************************************************
+     * \brief
+        Runs the iterative flood fill algorithm.
+     *
+     * \param pNode
+        The TreeNode of the starting node for flood fill.
+
+     * \param value
+        The string to fill the connected nodes with.
+    *******************************************************************************/
+    template<typename T>
+    inline void Flood_Fill_Iterative<T>::run(TreeNode* pNode, std::string value)
+    {
+        // Implement the flood fill
+        openlist.clear();
+        openlist.push(pNode);
+
+        while (!openlist.isEmpty())
+        {
+            TreeNode* current = openlist.pop();
+            std::vector<TreeNode*> adjacents = pGetAdjacents->operator()(current);
+
+            for (TreeNode* adj : adjacents)
+            {
+                adj->value = value;
+                openlist.push(adj);
+            }
+            //delete current;
+        }
+    }
+    template void Flood_Fill_Iterative<Queue>::run(TreeNode* pNode, std::string value);
+    template void Flood_Fill_Iterative<Stack>::run(TreeNode* pNode, std::string value);
+}
--- cs380_a3/functions.h
+++ cs380_a3/functions.h
@@ -1,3 +1,23 @@
+/*!*****************************************************************************
+\file functions.h
+\author Chen Yen Hsun
+\par DP email: c.yenhsun\@digipen.edu
+\par Course: CS380
+\par Section: A
+\par Programming Assignment 3
+\date 05-21-2023
+\brief
+
+Conbinaion of assignment 1 and 2, declard Node, GetAdjacents, 
+GetTreeAdjacents, Flood_Fill_Recursive class and template class
+Flood_Fill_Iterative template class functions and struct interface.
+Function definition for stack and queue
+
+Copyright (C) 2022 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of
+Technology is prohibited.
+*******************************************************************************/
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -25,6 +45,10 @@ namespace AI
         Node* parent;
         std::list<Node*> children;
 
+        /*!*****************************************************************************
+        \brief
+        Parameter constructor
+        *******************************************************************************/
         Node(T value = {}, Node* parent = nullptr,
             const std::list<Node*>& children = {})
             : value{ value }, parent{ parent }, children{ children }
@@ -32,38 +56,166 @@ namespace AI
 
         }
 
+        /*!*****************************************************************************
+        \brief
+        Destructor
+        *******************************************************************************/
         ~Node()
         {
             for (auto child : children)
                 delete child;
         }
 
-        // Serialization
+        /*!*****************************************************************************
+        \brief
+        Serialization operator for the `Node` struct template.
+
+        \param os
+        The output stream to write the serialized node to.
+
+        \param rhs
+        The node to be serialized.
+
+        \return
+        The reference to the output stream after serialization.
+        *******************************************************************************/
         friend std::ostream& operator<<(std::ostream& os, Node const& rhs)
         {
-            os << rhs.value; // just for test purpose
-            // ...
+            //os << rhs.value; // just for test purpose
+            //// ...
+
+            //return os;
+            os << rhs.value << " {";
+            os << rhs.children.size() << ' ';
+            for (auto const& child : rhs.children) {
+                os << child->getAsString();
+            }
+            os << "} ";
 
             return os;
         }
 
-        // Deserialization
+        /*!*****************************************************************************
+        \brief
+        Deserialization operator for the `Node` struct template.
+
+        \param is
+        The input stream to read the serialized node from.
+
+        \param rhs
+        The node to be deserialized.
+
+        \return
+        The reference to the input stream after deserialization.
+        *******************************************************************************/
         friend std::istream& operator>>(std::istream& is, Node& rhs)
         {
-            is >> rhs.value; // just for test purpose
-            // ...
+            //is >> rhs.value; // just for test purpose
+            //// ...
 
+            //return is;
+            std::string input;
+            std::getline(is, input);
+            rhs.setFromString(input);
             return is;
         }
+
+        /*!*****************************************************************************
+        \brief
+        Deserialization function that turns a stream of bytes
+        into an object in memory.
+
+        \param str
+        The string
+
+        \return
+        Returns a string
+        *******************************************************************************/
+        std::string setFromString(std::string str)
+        {
+
+            size_t n = str.find(' ');
+
+            if (n == std::string::npos)
+                return str;
+
+            this->value = str.substr(0, n);
+            str = str.substr(n + 1);
+
+            if (str[0] != '{')
+                return str;
+
+            str = str.substr(1);
+
+            n = str.find(' ');
+            if (n == std::string::npos)
+                return str;
+
+            int length;
+            bool check = isnumber(str.substr(0, n));
+
+            if (!check)
+                return str;
+            else
+                length = std::stoi(str.substr(0, n));
+
+            str = str.substr(n + 1);
+
+            for (int i = 0; i < length; ++i)
+            {
+                Node* child = new Node("", this);
+                str = child->setFromString(str);
+                this->children.push_back(child);
+            }
+
+            n = str.find('}');
+            if (n == std::string::npos)
+                return str;
+
+            str = str.substr(n + 2);
+
+            return str;
+        }
+
+        /*!*****************************************************************************
+        \brief
+        Checks whether a given string represents a number.
+
+        \param str
+        The string to be checked.
+
+        \return
+        Returns true if the string represents a number; otherwise, returns false.
+        *******************************************************************************/
+        static bool isnumber(const std::string& str);
 
         // Returns values from root to this node as an array
         std::vector<T> getPath() const
         {
-            std::vector<T> r;
-            // ...
+            //std::vector<T> r;
+            //// ...
 
+            //return r;
+            std::vector<T> r;
+            r.push_back(this->value);
+            Node* node = this->parent;
+            while (node)
+            {
+                r.insert(r.begin(), node->value);
+                node = node->parent;
+            }
             return r;
         }
+
+        /*!*****************************************************************************
+        \brief
+        Serialization function that turn a tree in memory into a stream
+        of bytes so it can be stored on disk or sent over the network.
+
+        \return
+        Returns a string representation of the node and its children.
+        *******************************************************************************/
+        std::string getAsString();
     };
 
 
@@ -90,118 +242,151 @@ namespace AI
     {
  
     public:
-
+        /*!*****************************************************************************
+        \brief
+        Default Constructor
+        *******************************************************************************/
         GetTreeAdjacents()
             : GetAdjacents()
         {
         }
 
-        std::vector<TreeNode*> operator()(TreeNode* pNode)
-        {
-           UNUSED(pNode)
-
-            std::vector<AI::TreeNode*> list = {};
-
-            // Push to the list all children of pNode excluding 
-            // those with value not equal "x"
-
-            return list;
-        }
+        /*!*****************************************************************************
+         * \brief
+            Functor operator that returns the adjacent nodes of a given key.
+         *
+         * \param pNode
+            The TreeNode for which adjacent nodes are to be retrieved.
+         *
+         * \return
+            A vector of pointers to AI::Node objects representing the adjacent nodes.
+        *******************************************************************************/
+        std::vector<TreeNode*> operator()(TreeNode* pNode);
     };
 
     // Domain specific functor that returns shuffled adjacent nodes
     class GetTreeStochasticAdjacents : public GetTreeAdjacents
     {
     public:
-
+        /*!*****************************************************************************
+        \brief
+        Default Constructor
+        *******************************************************************************/
         GetTreeStochasticAdjacents()
             : GetTreeAdjacents{}
         {
         }
 
-        std::vector<TreeNode*> operator()(TreeNode* pNode)
-        {
-            UNUSED(pNode)
-
-            std::vector<TreeNode*> adjacents;
-
-            // Use the base class operator() and then shuffle the result
-
-            return adjacents;
-        }
+        /*!*****************************************************************************
+         * \brief
+            Returns a vector of shuffled adjacent nodes in a map.
+         *
+         * \param pNode
+            The TreeNode of the node whose adjacent nodes are to be retrieved.
+         *
+         * \return
+            A vector of pointers to shuffled adjacent nodes in the map.
+        *******************************************************************************/
+        std::vector<TreeNode*> operator()(TreeNode* pNode);
     };
 
+    /*!*****************************************************************************
     // Wrappers that provide same interface for queue and stack
-
+    *******************************************************************************/
     struct Interface
     {
+        std::list<TreeNode*> p_Node;
+
         virtual void clear() = 0;
 
         virtual void push(TreeNode* pNode) = 0;
 
         virtual TreeNode* pop() = 0;
+
+        bool isEmpty() { return p_Node.size() == 0; }
     };
 
+    /*!*****************************************************************************
+    // Declare and define function for for struct Queue
+    *******************************************************************************/
     struct Queue : Interface //...
     {
         void clear()
         {
-            //...
+            p_Node.clear();
         }
 
         void push(TreeNode* pNode)
         {
-           UNUSED(pNode)
-           //...
+            p_Node.emplace_back(pNode);
         }
 
         TreeNode* pop()
         {
             TreeNode* pNode = nullptr;
-            //...
+            pNode = p_Node.front();
+            p_Node.pop_front();
+
             return pNode;
         }
     };
 
+    /*!*****************************************************************************
+    // Declare and define function for for struct Stack
+    *******************************************************************************/
     struct Stack : Interface //...
     {
         void clear()
         {
-            //...
+            p_Node.clear();
         }
 
         void push(TreeNode* pNode)
         {
-            UNUSED(pNode)
-            //...
+            p_Node.emplace_back(pNode);
         }
 
         TreeNode* pop()
         {
             TreeNode* pNode = nullptr;
-            //...
+            pNode = p_Node.back();
+            p_Node.pop_back();
+           
             return pNode;
         }
     };
 
-    // Recursive Flood Fill
+    /*!*****************************************************************************
+     * \brief Class for performing flood fill using a recursive algorithm.
+    *******************************************************************************/
     class Flood_Fill_Recursive
     {
         GetTreeAdjacents* pGetAdjacents;
 
     public:
+        /*!*****************************************************************************
+         * \brief
+            Constructs a `Flood_Fill_Recursive` object.
+         *
+         * \param pGetAdjacents
+            Pointer to a GetAdjacents object used for retrieving adjacent nodes.
+        *******************************************************************************/
         Flood_Fill_Recursive(GetTreeAdjacents* pGetAdjacents)
             : pGetAdjacents{ pGetAdjacents }
         {
         }
 
-        void run(TreeNode* pNode, std::string value)
-        {
-            UNUSED(pNode)
-            UNUSED(value)
+        /*!*****************************************************************************
+         * \brief
+            Runs the flood fill algorithm recursively.
+         *
+         * \param pNode
+            The pNode of the starting node for flood fill.
 
-            // Implement the flood fill
-        }
+         * \param value
+            The value to fill the connected nodes with.
+        *******************************************************************************/
+        void run(TreeNode* pNode, std::string value);
     };
 
     // Iterative Flood Fill
@@ -213,18 +398,29 @@ namespace AI
         T openlist;
 
     public:
+        /*!*****************************************************************************
+         * \brief
+            Constructs a `Flood_Fill_Recursive` object.
+         *
+         * \param pGetAdjacents
+            Pointer to a GetAdjacents object used for retrieving adjacent nodes.
+        *******************************************************************************/
         Flood_Fill_Iterative(GetTreeAdjacents* pGetAdjacents)
             : pGetAdjacents{ pGetAdjacents }, openlist{}
         {
         }
 
-        void run(TreeNode* pNode, std::string value)
-        {
-            UNUSED(pNode)
-            UNUSED(value)
+        /*!*****************************************************************************
+         * \brief
+            Runs the iterative flood fill algorithm.
+         *
+         * \param pNode
+            The TreeNode of the starting node for flood fill.
 
-            // Implement the flood fill
-         }
+         * \param value
+            The string to fill the connected nodes with.
+        *******************************************************************************/
+        void run(TreeNode* pNode, std::string value);
     };
 
 } // end namespace
